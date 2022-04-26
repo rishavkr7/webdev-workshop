b4d3e9d97833a249321bafa7fd784fc05ced800a
Renzil DSouza <renzil@gmail.com>
2022-04-26 05:52:53 +0000

    Adding global consts (API urls)

--- js/game.js
+++ js/game.js
@@ -1,4 +1,7 @@
-// JavaScript code for hollywood game
+// global consts
+const TRIVIA_API_URL = "https://opentdb.com/api.php?amount=1&category=11&difficulty=easy&type=multiple"; // see https://opentdb.com/api_config.php
+const TENOR_API_URL = "https://g.tenor.com/v1/search?q=QUERY&key=API_KEY&limit=10";
+const TENOR_API_KEY = ""; // you need to add your own API key from Tenor and replace API_KEY in TENOR_API_URL. See https://tenor.com/gifapi
 
 function on_page_load() {
     console.log("Page has loaded ...");
